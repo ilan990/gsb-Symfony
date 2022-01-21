e199e3d0e4d8e5adf9416405ed41a51f2fa4d898
Yewon <ga05019@naver.com>
2022-01-21 10:19:54 +0100

    fix database

--- migrations/Version20220103182457.php
+++ migrations/Version20220103182457.php
@@ -19,14 +19,12 @@ final class Version20220103182457 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE visiteur (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(50) NOT NULL, prenom VARCHAR(50) NOT NULL, adresse VARCHAR(255) NOT NULL, code_postal VARCHAR(5) NOT NULL, ville VARCHAR(100) NOT NULL, date_embauche DATE DEFAULT NULL, id_secteur INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user ADD is_verified TINYINT(1) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE visiteur');
         $this->addSql('ALTER TABLE user DROP is_verified');
     }
 }
