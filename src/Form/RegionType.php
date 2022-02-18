c19fbceb2ffcef39367737ba340d4cb4a34077e1
Yewon <ga05019@naver.com>
2022-02-18 11:52:58 +0100

    combobox complete

--- src/Form/RegionType.php
+++ src/Form/RegionType.php
@@ -6,13 +6,23 @@ use App\Entity\Region;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+use App\Entity\Secteur;
 
 class RegionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('code_secteur')
+            ->add('codeSecteur', EntityType::class, [
+                    'class' => Secteur::class,
+                    'choice_value' => 'id',
+                    'choice_label' => function (Secteur $sec) {
+                        return $sec->getLibelle();
+                    }
+                ]
+            )
             ->add('nom_region')
         ;
     }
--- src/Form/Visiteur2Type.php
+++ src/Form/Visiteur2Type.php
@@ -2,10 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Labo;
+use App\Entity\Secteur;
 use App\Entity\Visiteur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class Visiteur2Type extends AbstractType
 {
@@ -18,10 +22,25 @@ class Visiteur2Type extends AbstractType
             ->add('codePostal')
             ->add('ville')
             ->add('dateEmbauche')
-            ->add('codeSecteur')
-            ->add('laboCode')
-        ;
-    }
+            ->add('codeSecteur', EntityType::class, [
+                    'class' => Secteur::class,
+                    'choice_value' => 'id',
+                    'choice_label' => function (Secteur $sec) {
+                        return $sec->getLibelle();
+                    }
+                ]
+            )
+            ->add('laboCode', EntityType::class, [
+                    'class' => Labo::class,
+                    'choice_value' => 'id',
+                    'choice_label' => function (Labo $lab) {
+                        return $lab->getNom();
+                    }
+                ]
+            );}
+
+
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
